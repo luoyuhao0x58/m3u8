638f46374d48e1f280c3bde496c77402d9405a0f
Luo Yuhao <luoyuhao@haujilo.xyz>
2022-02-12 11:24:03 +0800

    feat: 修改CCTV-13源，并对所有源增加user-agent标识。

--- generate.py
+++ generate.py
@@ -3,6 +3,7 @@ import os
 import sys
 import csv
 
+AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36"
 TV_PATH = os.getenv("TV_PATH", './resources/tv.csv')
 
 def main(tv_path):
@@ -19,6 +20,7 @@ def main(tv_path):
             if e["logo"]:
                 tags.append(f'tvg-logo="{e["logo"]}"')
             print(f'#EXTINF:-1 {" ".join(tags)},{e["name"]}')
+            print(f'#EXTVLCOPT:http-user-agent={AGENT}')
             if e["referrer"]:
                 print(f'#EXTVLCOPT:http-referrer={e["referrer"]}')
             print(e["url"])
