f3c58ee7bc3eb49655474cf7b0a1f0924033fb4d
Luo Yuhao <luoyuhao@haujilo.xyz>
2023-02-05 12:56:46 +0800

    fix: 修复翡翠台。

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ import os
 import sys
 import csv
 
-AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.80 Safari/537.36"
+AGENT="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
 TV_PATH = os.getenv("TV_PATH", './resources/tv.csv')
 
 def main(tv_path):
